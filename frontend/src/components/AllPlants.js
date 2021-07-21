184a8b067f7312ee4ad631418f78bfe835fcf4f0
mylliejohnson <mylliejohnson@gmail.com>
2021-07-21 10:34:36 -0400

    plant grid

--- frontend/src/components/AllPlants.js
+++ frontend/src/components/AllPlants.js
@@ -15,49 +15,52 @@ function AllPlants(props) {
     return allPlants.map((eachPlant) => {
       return (
         <div key={eachPlant._id}>
-          <span className="img-wrapper">
-            <div>
-              <div>
-                {/* <span className="img-title">
-                  <br />
-                  {eachPlant.commonName}
-                  <br />({eachPlant.scientificName})
-                  <br />
-                </span> */}
-                {/* <p className="img-description">
-                  Care Leve: {eachPlant.careLevel}
-                  <br />
-                  Light: {eachPlant.light}
-                  <br />
-                  Water: {eachPlant.water}
-                  <br />
-                  Soil: {eachPlant.soil}
-                  <br />
-                  Humidity: {eachPlant.humidity}
-                  <br />
-                  GrowthHabit: {eachPlant.growthHabit}
-                  <br />
-                  Propagation: {eachPlant.propagation}
-                  <br />
-                  Position: {eachPlant.position}
-                  <br />
-                  Toxicity: {eachPlant.toxicity}
-                </p> */}
-              </div>
-            </div>
-            <img
-              className="gallery-grid"
-              src={eachPlant.image}
-              alt="green and growing"
-            />
-          </span>
+          <img
+            style={{ width: "100%" }}
+            className="plant-img"
+            src={eachPlant.image}
+            alt="green and growing"
+          />
         </div>
-      );
-    });
-  };
+      )
+    })
+  }
+
+  // const showPlantDetails = () => {
+  //   return allPlants.map((eachPlant) => {
+  //    return ( 
+  //     <div key={eachPlant._id} >
+  //       <span className="img-title">
+  //         <br />
+  //         {eachPlant.commonName}
+  //         <br />({eachPlant.scientificName})
+  //         <br />
+  //       </span>
+  //       <p className="img-description">
+  //         Care Leve: {eachPlant.careLevel}
+  //         <br />
+  //         Light: {eachPlant.light}
+  //         <br />
+  //         Water: {eachPlant.water}
+  //         <br />
+  //         Soil: {eachPlant.soil}
+  //         <br />
+  //         Humidity: {eachPlant.humidity}
+  //         <br />
+  //         GrowthHabit: {eachPlant.growthHabit}
+  //         <br />
+  //         Propagation: {eachPlant.propagation}
+  //         <br />
+  //         Position: {eachPlant.position}
+  //         <br />
+  //         Toxicity: {eachPlant.toxicity}
+  //       </p>
+  //     </div>
+  //    )});
+  // };
 
   return (
-    <div className="grid">
+    <div>
       <main>
         <ShowAllPlants />
       </main>
