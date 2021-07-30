ae8276939978c58bac71b9945e7051a651909150
leannewilson <83836254+leannewilson@users.noreply.github.com>
2021-07-30 07:10:41 -0700

    plant suggestions

--- frontend/src/components/PlantSuggestion.js
+++ frontend/src/components/PlantSuggestion.js
@@ -61,6 +61,7 @@ function PlantSuggestion(props) {
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
         padding: "100px",
+        height: "75vh",
       }}
     >
       <div className="container">
